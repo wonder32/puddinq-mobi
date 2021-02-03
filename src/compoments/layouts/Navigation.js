ebd4e54537711a1065a5e4fcb2ac410ddcc2abbb
Stefan Schotvanger <s.schotvanger@gmail.com>
2021-02-03 09:16:50 +0100

    menu 2/2

--- src/compoments/layouts/Navigation.js
+++ src/compoments/layouts/Navigation.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Navbar, Nav} from 'react-bootstrap';
 import styled from 'styled-components';
 import {NavLink} from 'react-router-dom';
@@ -8,14 +8,14 @@ const Navigation = () => {
 
     return (
         <NavigationContainer>
-            <Navbar bg="dark" expand="lg" className="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
-                <Navbar.Brand href="#home">puddinq &copy;</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="mr-auto">
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link as={NavLink} exact to="/">Home</Nav.Link>
-                        <Nav.Link as={NavLink} exact to="/map">Map</Nav.Link>
-                        <Nav.Link as={NavLink} exact to="/over">Over</Nav.Link>
+                        <Nav.Link href="/" exact to="/" as={NavLink}>Home</Nav.Link>
+                        <Nav.Link href="/map" exact to="/map" as={NavLink}>Map</Nav.Link>
+                        <Nav.Link href="/over" exact to="/over" as={NavLink}>Over</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
